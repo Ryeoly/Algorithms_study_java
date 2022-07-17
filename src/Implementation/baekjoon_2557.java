85923b600f681c67700a148e6f8acb93f21346c2
ryeoly2 <ryeoly2@gmail.com>
2022-07-18 08:25:58 +0900

    first commit

---
+++ src/Implementation/baekjoon_2557.java
@@ -0,0 +1,9 @@
+package Implementation;
+public class baekjoon_2557 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
