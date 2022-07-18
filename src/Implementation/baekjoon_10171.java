9179ce069ef2f1622b3817ec3a57b7409ebe1ebf
ryeoly2 <ryeoly2@gmail.com>
2022-07-19 08:55:09 +0900

    0719

---
+++ src/Implementation/baekjoon_10171.java
@@ -0,0 +1,13 @@
+package Implementation;
+
+public class baekjoon_10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
---
+++ src/Implementation/baekjoon_10172.java
@@ -0,0 +1,14 @@
+package Implementation;
+
+public class baekjoon_10172 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+
+}
