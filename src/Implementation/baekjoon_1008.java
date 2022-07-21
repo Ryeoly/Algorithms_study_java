7aa347ccc9eea4313a2d93974093eea041c96955
ryeoly2 <ryeoly2@gmail.com>
2022-07-21 23:07:16 +0900

    0721

---
+++ src/Implementation/baekjoon_1008.java
@@ -0,0 +1,13 @@
+package Implementation;
+import java.util.*;
+public class baekjoon_1008 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		System.out.println(a/b);
+		sc.close();
+	}
+}
+
