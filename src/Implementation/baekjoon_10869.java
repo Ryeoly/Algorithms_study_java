5502ccb39814557d42326e9d4d01db1b5203e258
ryeoly2 <ryeoly2@gmail.com>
2022-07-22 08:57:54 +0900

    0722

---
+++ src/Implementation/baekjoon_10869.java
@@ -0,0 +1,15 @@
+package Implementation;
+import java.util.*;
+public class baekjoon_10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
