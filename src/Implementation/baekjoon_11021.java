69cbca75f1ec9aa3ebe6b2aee5127d63036f2b83
ryeoly2 <ryeoly2@gmail.com>
2022-08-02 21:32:18 +0900

    0802

---
+++ src/Implementation/baekjoon_11021.java
@@ -0,0 +1,22 @@
+package Implementation;
+
+import java.io.*;
+import java.util.*;
+public class baekjoon_11021 {
+
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i< t ; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append("Case").append(" #").append(i+1).append(": ").append(a+b).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+}
